986fe8652a8d1aa687802e7e144c26ffa8fba7e3
은진 <ok94777@naver.com>
2024-11-27 22:30:23 +0900

    페이지 레이아웃 만들기

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
+
+  let name = 'park';
+  let link = 'http://google.com';
+
   return (
     <div>
-
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link} className="link">링크</a>
     </div>
   );
 }
