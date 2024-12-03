d210ead72116a751d010b35a1468ef448eb53523
은진 <ok94777@naver.com>
2024-12-03 22:02:56 +0900

    map 함수로 html 반복 생성하기

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,13 +1,18 @@
 export default function List() {
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+
     return (
         <dev>
             <h4 className="title">상품목록</h4>
-            <div className="food">
-                <h4>상품1 $40</h4>
-            </div>
-            <div className="food">
-                <h4>상품2 $40</h4>
-            </div>
+            {
+                상품.map((_,i) => {
+                    return (
+                        <div className="food">
+                            <h4>{상품[i]}</h4>
+                        </div>
+                    )
+                })
+            }
         </dev>
     )
 }
