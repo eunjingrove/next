ceb85ad2f8f16b4e2798348736adaced89b1c860
은진 <ok94777@naver.com>
2024-12-10 23:01:27 +0900

    props

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,22 +1,37 @@
 // 'use client'
 
 import {age, name} from './data.js'
+import Hi from './hi.js'
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', "Pasta"]
     return (
       <div>
+        <Hi/>
         <h4 className="title">Cart</h4>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
+        <CartItem item={장바구니[0]} />
+        <CartItem item={장바구니[1]}/>
+        <Banner content="롯데카드" />
+        <Banner content="현대카드" />
+        <Btn color='orange' />
       </div>
     )
-  } 
+  }  
 
-  function CartItem() {
+  function Btn(props) {
+    return ( 
+      <button style={{background : props.color}}>버튼</button>
+    )
+  }
+
+  function Banner(props) {
+    return <h5>{props.content} 결제 행사중</h5>
+  }
+
+  function CartItem(props) {
     return (
       <div className="cart-item">
-        <p>상품명 {age} </p>
+      <p>{props.item}</p>
         <p>$40</p>
         <p>1개</p>
     </div>
