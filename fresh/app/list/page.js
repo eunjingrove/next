f79cdea23f2bcd3dbde6cdced28aeab582ff85ed
은진 <ok94777@naver.com>
2024-12-11 22:36:52 +0900

    useState, onClick

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,8 +1,12 @@
+'use client'
+
 import Image from "next/image"
+import { useState } from "react"
 // import TomatoesImg from '/public/food0.png' 이미지 최적화 
 
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let [수량,수량변경] = useState(0);
 
     return (
         <dev>
@@ -13,6 +17,9 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} alt="토마토" className="food-img" />
                             <h4>{상품[i]}</h4>
+                            <span> {수량} </span>
+                            <button onClick={() => { 수량변경(수량+1)}}>+</button>
+                            <button onClick={() => { 수량변경(수량-1)}}>-</button>
                         </div>
                     )
                 })
