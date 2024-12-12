228d4e15a5c3f29efeee952fd909ac9187bdc4cd
은진 <ok94777@naver.com>
2024-12-12 22:33:10 +0900

    array/object state 수정

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -6,20 +6,34 @@ import { useState } from "react"
 
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-    let [수량,수량변경] = useState(0);
+    let [수량,수량변경] = useState([0, 0, 0]);
 
     return (
         <dev>
             <h4 className="title">상품목록</h4>
+            <span>{수량[0]}</span>
+            <button onClick={() => {
+                let copy = [...수량]
+                copy[0]++
+                수량변경(copy)
+            }}>+</button>
             {
                 상품.map((_,i) => {
                     return (
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} alt="토마토" className="food-img" />
                             <h4>{상품[i]}</h4>
-                            <span> {수량} </span>
-                            <button onClick={() => { 수량변경(수량+1)}}>+</button>
-                            <button onClick={() => { 수량변경(수량-1)}}>-</button>
+                            <span> {수량[i]} </span>
+                            <button onClick={() => {
+                                let copy = [...수량]
+                                copy[i]++
+                                수량변경(copy)
+                            }}>+</button>
+                            <button onClick={() => {
+                                let copy = [...수량]
+                                copy[i]--
+                                수량변경(copy)
+                            }}>-</button>
                         </div>
                     )
                 })
